d13e41c8f8d41e0dbccd49b78ed58c21cf55cba5
AdityaDNikam <aditya.prakash.nikam.2004@gmail.com>
2026-06-23 00:46:00 +0530

    fix: correct firebase casing mismatch, JSX conditional rendering, and style AddContact overlay modal

--- Proj 5 Firebase contact manager/src/App.jsx
+++ Proj 5 Firebase contact manager/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -8,17 +8,53 @@ import Navbar from './components/Navbar'
 import searchBarContaxt from './assets/context/Search_barContext'
 import ContactDet from './components/ContactDet'
 import AddContact from './components/AddContact'
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "./Config/firebase"
 
 function App() {
   const [search, setSearch] = useState(null)
+  const [contact, setContact] = useState([])
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactRef = collection(db, "Contacts")
+        const contactData = await getDocs(contactRef);
+        const contact_list = contactData.docs.map((docs) => {
+          return {
+            id: docs.id,
+            ...docs.data(),
+          };
+        })
+        setContact(contact_list);
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+    getContacts();
+
+  }, [])
 
   return (
     <searchBarContaxt.Provider value={[search, setSearch]}><div className="bg-black h-screen w-full flex justify-center">
-      <div className='bg-gray-900 w-[500px] h-screen flex flex-col'>
+      <div className='bg-gray-900 w-[500px] h-screen flex flex-col relative'>
         <Navbar />
-        <ContactDet />
+        {/* Conditionally rendering ContactDet and AddContacts*/}
+        {contact.length === 0 ? (
+          <NoContacts />
+        ) : (
+          contact.map((contacts) => (
+            <ContactDet
+              key={contacts.id}
+              name={contacts.Name}
+              email={contacts.Email}
+            />
+          ))
+        )}
+
         {/* <NoContacts /> */}
-        <AddContact />
+        {/* <AddContact /> */}
       </div>
     </div>
     </searchBarContaxt.Provider>
--- Proj 5 Firebase contact manager/src/components/ContactDet.jsx
+++ Proj 5 Firebase contact manager/src/components/ContactDet.jsx
@@ -3,14 +3,14 @@ import ProfileHolder from "../assets/Photos/ph_user-circle-thin.png"
 import EditIcon from "../assets/Photos/mdi_circle-edit-outline.png"
 import DeleteIcon from "../assets/Photos/mdi_trash-can-empty.png"
 
-function ContactDet() {
+function ContactDet({ name, email }) {
     return (
         <div className="Contact-Det-Main max-h-[53px] flex justify-between items-center row p-[10px] w-[460px] bg-white rounded-md my-[10px] mx-[20px]">
             <div className='Profile icon flex row jsutify-start '>
                 <img src={ProfileHolder} alt="Profile Placeholder" className='mr-[5px]' />
                 <div className='Contact Info'>
-                    <p>Aditya Nikam</p>
-                    <p>Aditya.Nikam@GOAT.com</p>
+                    <p>{name}</p>
+                    <p>{email}</p>
                 </div>
             </div>
             <div className='flex justify-end'>
