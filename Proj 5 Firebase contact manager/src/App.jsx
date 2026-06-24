a5170ce2c9b8441eff35da3fb1617aa56d77b7ab
AdityaDNikam <aditya.prakash.nikam.2004@gmail.com>
2026-06-24 23:46:26 +0530

    Project Ready

--- Proj 5 Firebase contact manager/src/App.jsx
+++ Proj 5 Firebase contact manager/src/App.jsx
@@ -6,58 +6,66 @@ import './App.css'
 import NoContacts from './components/NoContacts'
 import Navbar from './components/Navbar'
 import searchBarContaxt from './assets/context/Search_barContext'
+import AddForm_Context from './assets/context/AddForm_Context'
 import ContactDet from './components/ContactDet'
-import AddContact from './components/AddContact'
-import { collection, getDocs } from "firebase/firestore"
+import AddContact from './components/ActionContact'
+import { collection, onSnapshot } from "firebase/firestore"
 import { db } from "./Config/firebase"
 
 function App() {
   const [search, setSearch] = useState(null)
   const [contact, setContact] = useState([])
+  const [clicked, setClicked] = useState(false)
+  const [open, setOpen] = useState({ isOpen: false, contact: null })
 
   useEffect(() => {
-    const getContacts = async () => {
-      try {
-        const contactRef = collection(db, "Contacts")
-        const contactData = await getDocs(contactRef);
-        const contact_list = contactData.docs.map((docs) => {
-          return {
-            id: docs.id,
-            ...docs.data(),
-          };
-        })
-        setContact(contact_list);
-      }
-      catch (error) {
-        console.log(error)
-      }
-    }
-    getContacts();
+    const contactRef = collection(db, "Contacts")
+    const unsubscribe = onSnapshot(contactRef, (snapshot) => {
+      const contact_list = snapshot.docs.map((docs) => {
+        return {
+          id: docs.id,
+          ...docs.data(),
+        };
+      })
+      setContact(contact_list);
+    }, (error) => {
+      console.log(error);
+    });
 
+    return () => unsubscribe();
   }, [])
 
-  return (
-    <searchBarContaxt.Provider value={[search, setSearch]}><div className="bg-black h-screen w-full flex justify-center">
-      <div className='bg-gray-900 w-[500px] h-screen flex flex-col relative'>
-        <Navbar />
-        {/* Conditionally rendering ContactDet and AddContacts*/}
-        {contact.length === 0 ? (
-          <NoContacts />
-        ) : (
-          contact.map((contacts) => (
-            <ContactDet
-              key={contacts.id}
-              name={contacts.Name}
-              email={contacts.Email}
-            />
-          ))
-        )}
+  const filteredContacts = contact.filter((c) => {
+    if (!search) return true;
+    return c.Name?.toLowerCase().includes(search.toLowerCase());
+  });
 
-        {/* <NoContacts /> */}
-        {/* <AddContact /> */}
-      </div>
-    </div>
-    </searchBarContaxt.Provider>
+  return (
+    <AddForm_Context.Provider value={[open, setOpen]}>
+      <searchBarContaxt.Provider value={[search, setSearch]}>
+        <div className="bg-black h-screen w-full flex justify-center">
+          <div className='bg-gray-900 w-[500px] h-screen flex flex-col relative'>
+            <Navbar />
+            {/* Conditionally rendering ContactDet and AddContacts*/}
+            {filteredContacts.length === 0 ? (
+              <NoContacts />
+            ) : (
+              filteredContacts.map((contacts) => (
+                <ContactDet
+                  key={contacts.id}
+                  id={contacts.id}
+                  name={contacts.Name}
+                  email={contacts.Email}
+                />
+              ))
+            )}
+            {open.isOpen ? (
+              <AddContact />
+            ) : null}
+          </div>
+        </div>
+      </searchBarContaxt.Provider>
+    </AddForm_Context.Provider>
   )
 }
 
